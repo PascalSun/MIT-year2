4b0c92694c3885b59747f732d8088c4ee309fc15
PascalSun <sunqiang93@163.com>
2017-04-27 17:37:49 +0800

    mean blog

--- AgileWebDevelopment/learning/myblog/index.js
+++ AgileWebDevelopment/learning/myblog/index.js
@@ -6,7 +6,8 @@ var flash = require('connect-flash');
 var config = require('config-lite')(__dirname);
 var routes = require('./routes');
 var pkg = require('./package');
-
+var winston = require('winston');
+var expressWinston = require('express-winston');
 var app = express();
 
 // 设置模板目录
@@ -50,9 +51,44 @@ app.use(function (req, res, next) {
   next();
 });
 // 路由
+// 正常请求的日志
+app.use(expressWinston.logger({
+  transports: [
+    new (winston.transports.Console)({
+      json: true,
+      colorize: true
+    }),
+    new winston.transports.File({
+      filename: 'logs/success.log'
+    })
+  ]
+}));
+// 路由
 routes(app);
-
-// 监听端口，启动程序
-app.listen(config.port, function () {
-  console.log(`${pkg.name} listening on port ${config.port}`);
+// 错误请求的日志
+app.use(expressWinston.errorLogger({
+  transports: [
+    new winston.transports.Console({
+      json: true,
+      colorize: true
+    }),
+    new winston.transports.File({
+      filename: 'logs/error.log'
+    })
+  ]
+}));
+// error page
+app.use(function (err, req, res, next) {
+  res.render('error', {
+    error: err
+  });
 });
+// 监听端口，启动程序
+if (module.parent) {
+  module.exports = app;
+} else {
+  // 监听端口，启动程序
+  app.listen(config.port, function () {
+    console.log(`${pkg.name} listening on port ${config.port}`);
+  });
+}
