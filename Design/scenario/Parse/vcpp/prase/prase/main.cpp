426ad47884670ab80a7622f413d9a0cbd687e601
PascalSun <sunqiang93@163.com>
2017-05-17 19:57:49 +0800

    commit

---
+++ Design/scenario/Parse/vcpp/prase/prase/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  prase
+//
+//  Created by Pascal_Sun on 17/5/17.
+//  Copyright © 2017 Pascal_Sun. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
